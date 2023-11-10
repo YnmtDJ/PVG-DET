e0f7a44cd895454e82ba72090f473e08fba99bb2
YnmtDJ <hu.nan@cmge.com>
2023-11-10 18:55:22 +0800

    feat: add "dataset/" into .gitignore.

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -7,8 +7,8 @@ import torchvision.datasets
 def create_dataset(opts):
 
     if opts.datasetName == "COCO":
-        dataset = torchvision.datasets.CocoDetection(root=os.path.join(opts.datasetRoot, "COCO", "2017"),
-                                                     annFile=os.path.join(opts.datasetRoot, "COCO", "annotations", "2017.json"))
+        dataset = torchvision.datasets.CocoDetection(root=os.path.join(opts.datasetRoot, "COCO/images/val2017"),
+                                                     annFile=os.path.join(opts.datasetRoot, "COCO/annotations/instances_val2017.json"))
     elif opts.datasetName == "ImageNet":
         dataset = torchvision.datasets.ImageNet(root=os.path.join(opts.datasetRoot, "ImageNet"))
     else:
