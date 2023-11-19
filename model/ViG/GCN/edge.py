9aa1907ef78d31a0f058650f88f61d78abad3629
YnmtDJ <1624309321@qq.com>
2023-11-20 01:10:58 +0800

    feat: add edge.py to find the neighbors' indices based on dilated knn.

---
+++ model/ViG/GCN/edge.py
@@ -0,0 +1,64 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+
+class DenseDilatedKnnGraph(nn.Module):
+    """
+    Find the neighbors' indices based on dilated knn
+    """
+    def __init__(self, k=9, dilation=1):
+        super(DenseDilatedKnnGraph, self).__init__()
+        self.dilation = dilation
+        self.k = k
+
+    def forward(self, x):
+        x = F.normalize(x, p=2.0, dim=1)  # (batch_size, num_dims, num_points, 1)
+        edge_index = dense_knn_matrix(x, self.k * self.dilation)  # (2, batch_size, num_points, k*dilation)
+        edge_index = edge_index[:, :, :, ::self.dilation]  # (2, batch_size, num_points, k)
+        return edge_index
+
+
+def dense_knn_matrix(x, k=16):
+    """
+    Get KNN based on the pairwise distance.
+    :param x: (batch_size, num_dims, num_points, 1)
+    :param k: The number of neighbors
+    :return: The indices of nearest neighbors and center - (2, batch_size, num_points, k)
+    """
+    with torch.no_grad():
+        x = x.transpose(2, 1).squeeze(-1)
+        batch_size, n_points, n_dims = x.shape
+        # memory efficient implementation
+        n_part = 10000
+        if n_points > n_part:
+            # TODO: implement memory efficient knn
+            nn_idx_list = []
+            # groups = math.ceil(n_points / n_part)
+            # for i in range(groups):
+            #     start_idx = n_part * i
+            #     end_idx = min(n_points, n_part * (i + 1))
+            #     dist = part_pairwise_distance(x.detach(), start_idx, end_idx)
+            #     if relative_pos is not None:
+            #         dist += relative_pos[:, start_idx:end_idx]
+            #     _, nn_idx_part = torch.topk(-dist, k=k)
+            #     nn_idx_list += [nn_idx_part]
+            # nn_idx = torch.cat(nn_idx_list, dim=1)  # (batch_size, num_points, k)
+        else:
+            dist = pairwise_distance(x.detach())  # (batch_size, num_points, num_points)
+            _, nn_idx = torch.topk(-dist, k=k)  # (batch_size, num_points, k)
+        center_idx = torch.arange(0, n_points, device=x.device).repeat(batch_size, k, 1).transpose(2, 1)
+    return torch.stack((nn_idx, center_idx), dim=0)  # (2, batch_size, num_points, k)
+
+
+def pairwise_distance(x):
+    """
+    Compute pairwise distance of a point cloud.
+    :param x: (batch_size, num_points, num_dims)
+    :return: pairwise distance - (batch_size, num_points, num_points)
+    """
+    with torch.no_grad():
+        x_inner = -2*torch.matmul(x, x.transpose(2, 1))  # (batch_size, num_points, num_points)
+        x_square = torch.sum(torch.mul(x, x), dim=-1, keepdim=True)  # (batch_size, num_points, 1)
+        return x_square + x_inner + x_square.transpose(2, 1)
+
--- model/ViG/GCN/grapher.py
+++ model/ViG/GCN/grapher.py
@@ -1,4 +1,8 @@
+import torch
 from torch import nn
+from torch.functional import F
+
+from model.ViG.GCN.edge import DenseDilatedKnnGraph
 
 
 class Grapher(nn.Module):
@@ -50,49 +54,44 @@ class Grapher(nn.Module):
         return x
 
 
+class GraphConv2d(nn.Module):
+    """
+    Static graph convolution layer
+    """
+    def __init__(self, in_ch, out_ch, conv='mr', act=nn.GELU(), norm=None, bias=True):
+        super(GraphConv2d, self).__init__()
+        if conv == 'mr':
+            self.gcn = MRConv2d(in_ch, out_ch, act, norm, bias)
+        elif conv == 'edge':
+            self.gcn = EdgeConv2d(in_ch, out_ch, act, norm, bias)
+        elif conv == 'sage':
+            self.gcn = GraphSAGE(in_ch, out_ch, act, norm, bias)
+        elif conv == 'gin':
+            self.gcn = GINConv2d(in_ch, out_ch, act, norm, bias)
+        else:
+            raise NotImplementedError('gcn:{} is not supported'.format(conv))
+
+    def forward(self, x, edge_index, y=None):
+        return self.gcn(x, edge_index, y)
+
+
 class DyGraphConv2d(GraphConv2d):
     """
     Dynamic graph convolution layer
     """
     def __init__(self, in_channels, out_channels, kernel_size=9, dilation=1, conv='edge', act='relu',
-                 norm=None, bias=True, stochastic=False, epsilon=0.0, r=1):
+                 norm=None, bias=True):
         super(DyGraphConv2d, self).__init__(in_channels, out_channels, conv, act, norm, bias)
         self.k = kernel_size
         self.d = dilation
-        self.r = r
-        self.dilated_knn_graph = DenseDilatedKnnGraph(kernel_size, dilation, stochastic, epsilon)
+        self.dilated_knn_graph = DenseDilatedKnnGraph(kernel_size, dilation)
 
-    def forward(self, x, relative_pos=None):
+    def forward(self, x):
         B, C, H, W = x.shape
-        y = None
-        if self.r > 1:
-            y = F.avg_pool2d(x, self.r, self.r)
-            y = y.reshape(B, C, -1, 1).contiguous()
         x = x.reshape(B, C, -1, 1).contiguous()
-        edge_index = self.dilated_knn_graph(x, y, relative_pos)
-        x = super(DyGraphConv2d, self).forward(x, edge_index, y)
+        edge_index = self.dilated_knn_graph(x)
+        x = super(DyGraphConv2d, self).forward(x, edge_index)
         return x.reshape(B, -1, H, W).contiguous()
-
-
-class GraphConv2d(nn.Module):
-    """
-    Static graph convolution layer
-    """
-    def __init__(self, in_channels, out_channels, conv='edge', act='relu', norm=None, bias=True):
-        super(GraphConv2d, self).__init__()
-        if conv == 'edge':
-            self.gconv = EdgeConv2d(in_channels, out_channels, act, norm, bias)
-        elif conv == 'mr':
-            self.gconv = MRConv2d(in_channels, out_channels, act, norm, bias)
-        elif conv == 'sage':
-            self.gconv = GraphSAGE(in_channels, out_channels, act, norm, bias)
-        elif conv == 'gin':
-            self.gconv = GINConv2d(in_channels, out_channels, act, norm, bias)
-        else:
-            raise NotImplementedError('conv:{} is not supported'.format(conv))
-
-    def forward(self, x, edge_index, y=None):
-        return self.gconv(x, edge_index, y)
 
 
 class MRConv2d(nn.Module):
@@ -100,31 +99,30 @@ class MRConv2d(nn.Module):
     Max-Relative Graph Convolution.
     https://arxiv.org/abs/1904.03751
     """
-    def __init__(self, in_ch, out_ch, act=nn.GELU(), dropout=0.0):
+    def __init__(self, in_ch, out_ch, act=nn.GELU()):
         """
         :param in_ch: The number of input channels.
         :param out_ch: The number of output channels.
         :param act: The activation function.
-        :param dropout: Probability of an element to be zero-ed.
         """
         super(MRConv2d, self).__init__()
         self.conv = nn.Sequential(
             nn.Conv2d(in_ch*2, out_ch, 1, groups=4),
             nn.BatchNorm2d(out_ch),
             act,
-            nn.Dropout2d(dropout)
         )
 
-    def forward(self, x, edge_index, y=None):
-        x_i = batched_index_select(x, edge_index[1])
-        if y is not None:
-            x_j = batched_index_select(y, edge_index[0])
-        else:
-            x_j = batched_index_select(x, edge_index[0])
-        x_j, _ = torch.max(x_j - x_i, -1, keepdim=True)
+    def forward(self, x, edge_index):
+        """
+        :param x: (batch_size, num_dims, num_points, 1)
+        :param edge_index: (2, batch_size, num_points, k)
+        """
+        x_i = batched_index_select(x, edge_index[1])  # center (batch_size, num_dims, num_points, k)
+        x_j = batched_index_select(x, edge_index[0])  # neighbors (batch_size, num_dims, num_points, k)
+        x_j, _ = torch.max(x_j - x_i, dim=-1, keepdim=True)  # (batch_size, num_dims, num_points, 1)
         b, c, n, _ = x.shape
-        x = torch.cat([x.unsqueeze(2), x_j.unsqueeze(2)], dim=2).reshape(b, 2 * c, n, _)
-        return self.nn(x)
+        x = torch.cat([x, x_j], dim=1)
+        return self.conv(x)
 
 
 class EdgeConv2d(nn.Module):
@@ -183,24 +181,27 @@ class GINConv2d(nn.Module):
 
 
 def batched_index_select(x, idx):
-    """fetches neighbors features from a given neighbor idx
-
-    Args:
-        x (Tensor): input feature Tensor
-                :math:`\mathbf{X} \in \mathbb{R}^{B \times C \times N \times 1}`.
-        idx (Tensor): edge_idx
-                :math:`\mathbf{X} \in \mathbb{R}^{B \times N \times l}`.
-    Returns:
-        Tensor: output neighbors features
-            :math:`\mathbf{X} \in \mathbb{R}^{B \times C \times N \times k}`.
     """
-    batch_size, num_dims, num_vertices_reduced = x.shape[:3]
-    _, num_vertices, k = idx.shape
-    idx_base = torch.arange(0, batch_size, device=idx.device).view(-1, 1, 1) * num_vertices_reduced
+    Fetches neighbors features from a given neighbor idx
+    :param x: Input feature Tensor (batch_size, num_dims, num_points, 1)
+    :param idx: Edge_idx (batch_size, num_points, k)
+    :return: Neighbors features (batch_size, num_dims, num_points, k)
+    """
+
+    """
+    Turn the tensor into a one-dimensional, 
+    so the idx in each batch increases batch_size from the previous batch
+    x - (batch_size*num_points, num_dims)
+    idx - (batch_size*num_points*k)
+    feature - (batch_size*num_points*k, num_dims)
+    """
+    batch_size, num_dims, num_points = x.shape[:3]
+    _, _, k = idx.shape
+    idx_base = torch.arange(0, batch_size, device=idx.device).view(-1, 1, 1) * num_points
     idx = idx + idx_base
     idx = idx.contiguous().view(-1)
 
     x = x.transpose(2, 1)
-    feature = x.contiguous().view(batch_size * num_vertices_reduced, -1)[idx, :]
-    feature = feature.view(batch_size, num_vertices, k, num_dims).permute(0, 3, 1, 2).contiguous()
+    feature = x.contiguous().view(batch_size * num_points, -1)[idx, :]
+    feature = feature.view(batch_size, num_points, k, num_dims).permute(0, 3, 1, 2).contiguous()
     return feature
