f37654dd8ca1a1c21ce7a3423f3a80e1aaaa1626
YnmtDJ <hu.nan@cmge.com>
2023-11-24 19:06:46 +0800

    feat: add training parameters.

--- train.py
+++ train.py
@@ -1,12 +1,24 @@
-import torchvision
-import torch
+import os
 
-from dataset.dataset import create_dataset
+from torch.utils.data import DataLoader
+
+from dataset.dataset import create_coco_dataset
 from util.option import get_opts
 
 if __name__ == "__main__":
 
     opts = get_opts()
 
-    dataset = create_dataset(opts)
+    coco_root = os.path.join(opts.datasetRoot, opts.datasetName, "images/val2017")
+    coco_annFile = os.path.join(opts.datasetRoot, opts.datasetName, "annotations/instances_val2017.json")
+    dataset = create_coco_dataset(coco_root, coco_annFile, True)
+    dataloader = DataLoader(dataset, batch_size=opts.batchSize, shuffle=True, drop_last=False)
+
+    for i, (images, targets) in enumerate(dataloader):
+        print(images.shape)
+        print(targets)
+        break
+    
+    
+
 
--- util/option.py
+++ util/option.py
@@ -8,4 +8,9 @@ def get_opts():
     parser.add_argument("--datasetRoot", type=str, default="dataset", help="the root directory of dataset")
     parser.add_argument("--datasetName", type=str, default="COCO", help="select the dataset to use")
 
+    # model parameters
+
+    # training parameters
+    parser.add_argument("--batchSize", type=int, default=8, help="batch size")
+
     return parser.parse_args()
